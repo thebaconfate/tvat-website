3abe356d4fa25017c5d79307a6cd1c4c75dc3eb9
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-10-23 14:53:59 +0200

    renamed login and added routes

---
+++ src/lib/routes.ts
@@ -0,0 +1,14 @@
+const websiteRoutes = {
+  home: { url: "/" },
+  lustrumGala: { url: "/lustrumgalabal" },
+  board: { url: "/bestuur" },
+  clubsong: { url: "/clublied" },
+  history: { url: "/history" },
+  krambambouli: { url: "/krambambouli" },
+  codex: { url: "/codex", challenge: { url: "/challenge" } },
+  youtube: { url: "https://www.youtube.com/channel/UCm0gsmr5OWyS_0LNXkIqmBw" },
+  mailingslist: { url: "/mailingslijst", enroll: { url: "/inschrijven" } },
+  membership: { url: "/lidkaarten" },
+  contact: { url: "/contact" },
+  login: { url: "/login" },
+};
