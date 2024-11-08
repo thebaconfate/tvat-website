e1d97d9296b01e2f1b52512b4b92dc96b3dbbeef
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-11-08 18:44:32 +0100

    added node SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/home": "/",
   },
+
   output: "hybrid",
   integrations: [react()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
