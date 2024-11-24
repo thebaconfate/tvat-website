675fee12660829656a30b30de3a9fcb855782f5b
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2024-11-24 20:17:10 +0100

    bugfix 2, endpoint now returns response

--- src/pages/api/krambambouli/orders.ts
+++ src/pages/api/krambambouli/orders.ts
@@ -11,11 +11,12 @@ export async function GET({}) {
   const result = (await Database.getInstance().then((database) => {
     return database.getKrambambouliOrders();
   })) as Order[];
-  return result.reduce(
+  const orders = result.reduce(
     (prev, current) => {
       if (current.productId === 1) return { ...prev, classic: prev.classic++ };
       else return { ...prev, minus: prev.minus++ };
     },
     { classic: 0, minus: 0 },
   );
+  return new Response(JSON.stringify(orders), { status: 200 });
 }
