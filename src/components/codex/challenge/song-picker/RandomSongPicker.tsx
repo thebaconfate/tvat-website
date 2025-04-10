dafab70d9f171a6bbf79ccbc9cc00b0d712f36b2
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2025-04-10 03:44:43 +0200

    added styles

---
+++ src/components/codex/challenge/song-picker/RandomSongPicker.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import { Language, type Song } from "../../../../lib/codex-challenge"
+import "./random-song-picker.css"
+
+interface Props {
+    songs: Song[]
+}
+
+function fmt(song: Song) {
+    return `Titel: ${song.title}\nPagina: ${song.page}` + (song.description ? song.description : "")
+}
+
+export default function RandomSongPicker({ songs }: Props) {
+    console.log(songs)
+    const [song, setSong] = useState<Song | null>(null)
+    const dutchSongs = songs.filter((song) => song.language == Language.DUTCH)
+    const frenchSongs = songs.filter((song) => song.language == Language.FRENCH)
+    const germanSongs = songs.filter((song) => song.language == Language.GERMAN)
+    const otherSongs = songs.filter((song) => (song.language.includes(Language.ENGLISH)) || (song.language.includes(Language.OTHER)))
+
+    console.log(germanSongs)
+
+    function makeRandomFunction(langlist: Song[]) {
+        return (_: React.MouseEvent<HTMLButtonElement>) => {
+            const randIdx = Math.floor(Math.random() * langlist.length)
+            setSong(langlist[randIdx])
+        }
+    }
+
+
+    return <div className="picker">
+        <div className="buttons">
+            <button className="lang" onClick={makeRandomFunction(dutchSongs)}>Nederlands</button>
+            <button className="lang" onClick={makeRandomFunction(frenchSongs)}>Frans</button>
+            <button className="lang" onClick={makeRandomFunction(germanSongs)}>Duits</button>
+            <button className="lang" onClick={makeRandomFunction(otherSongs)}>Engels en anderstalig</button>
+        </div>
+        <div className="result">
+            {
+                song ? <div className="song">
+                    <p>Titel: {song.title}</p>
+                    <p>Pagina: {song.page}</p>
+                    {
+                        song.description ? <p>{song.description}</p> : ""
+                    }
+
+                </div> : ""
+            }
+        </div>
+    </div>
+
+
+}
