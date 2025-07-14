61ff9a5aaeb4a718902d5a18b42cebf18fefc778
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2025-07-14 22:47:17 +0200

    implemented challenge for pc

--- src/components/codex/challenge/codex-challenge/CodexChallenge.tsx
+++ src/components/codex/challenge/codex-challenge/CodexChallenge.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import type { Song } from "../../../../lib/codex-challenge";
+import { Language, type Song } from "../../../../lib/codex-challenge";
 import RandomSongPicker from "../song-picker/RandomSongPicker";
 import "./codex-challenge.css";
 
@@ -15,73 +15,137 @@ enum Mode {
   Randomizer,
 }
 
+type Score = { [key in Language]: number };
+const initScores: Score = {
+  [Language.DUTCH]: 0,
+  [Language.FRENCH]: 0,
+  [Language.GERMAN]: 0,
+  [Language.ENGLISH]: 0,
+  [Language.OTHER]: 0,
+};
+
 export default function CodexChallenge({
   dutchSongs,
   frenchSongs,
   germanSongs,
   otherSongs,
 }: Props) {
-  const [mode, setMode] = useState<Mode | undefined>();
-  const [score, setScore] = useState<number>(0);
+  const [mode, setMode] = useState<Mode | undefined>(Mode.CodexChallenge);
+  const [totalScore, setTotalScore] = useState<Score>(initScores);
   const [song, setSong] = useState<Song | undefined>(undefined);
+  const [songSet, setSongSet] = useState<Set<Song>>(new Set<Song>());
+  const [addedScore, setAddedScore] = useState<boolean>(false);
+
+  function goBack(_: any) {
+    setMode(undefined);
+    setTotalScore(initScores);
+    setSong(undefined);
+    setSongSet(new Set<Song>());
+    setAddedScore(false);
+  }
+
+  function createScoreHandler(score: number) {
+    return (_: any) => {
+      if (song !== undefined && !addedScore) {
+        setTotalScore({
+          ...totalScore,
+          [song.language]: totalScore[song.language] + score,
+        });
+        setAddedScore(true);
+      }
+    };
+  }
 
   return (
     <>
-      {mode === Mode.Randomizer ? (
-        <RandomSongPicker
-          dutchSongs={dutchSongs}
-          frenchSongs={frenchSongs}
-          germanSongs={germanSongs}
-          otherSongs={otherSongs}
-          song={song}
-          setSong={setSong}
-        />
-      ) : mode == Mode.CodexChallenge ? (
-        <>
-          <RandomSongPicker
-            dutchSongs={dutchSongs}
-            frenchSongs={frenchSongs}
-            germanSongs={germanSongs}
-            otherSongs={otherSongs}
-            song={song}
-            setSong={setSong}
-          ></RandomSongPicker>
-        </>
-      ) : (
-        <div className="content-container">
-          <div className="information-container">
-            <h1 className="title">Codex Challenge</h1>
-            <p>Welkom tot de codex challenge </p>
-            <p>
-              Voor de challenge wordt gevraagd om de eerste strofe en het
-              refrein te zingen zonder codex, tenzij anders vermeld.
-            </p>
-            <p>
-              Elk lied staat op 2 punten en er worden 10 liedjes gevraagd. Je
-              kan dus 20 punten in totaal scoren.
-            </p>
-            <p>Je slaagt als je minstens 10 op 20 haalt.</p>
-            <p>
-              Er worden 4 nederland-, 3 frans-, 2 duits- en 1 anders- of
-              engelstalige liedjes gevraagd.
-            </p>
-          </div>
-          <div className="button-container">
-            <button
-              className="challenge-button"
-              onClick={(_) => setMode(Mode.CodexChallenge)}
-            >
-              Challenge starten
-            </button>
-            <button
-              className="randomizer-button"
-              onClick={(_) => setMode(Mode.Randomizer)}
-            >
-              Oefenen
-            </button>
-          </div>
-        </div>
-      )}
+      <div className="content-container">
+        <h1 className="title">Codex Challenge</h1>
+        {mode === Mode.Randomizer ? (
+          <>
+            <RandomSongPicker
+              dutchSongs={dutchSongs}
+              frenchSongs={frenchSongs}
+              germanSongs={germanSongs}
+              otherSongs={otherSongs}
+              song={song}
+              setSong={setSong}
+            />
+            <div className="return-button-container">
+              <button className="return-button" onClick={goBack}>
+                Terug
+              </button>
+            </div>
+          </>
+        ) : mode == Mode.CodexChallenge ? (
+          <>
+            <RandomSongPicker
+              dutchSongs={dutchSongs}
+              frenchSongs={frenchSongs}
+              germanSongs={germanSongs}
+              otherSongs={otherSongs}
+              song={song}
+              alreadySelectedSongs={songSet}
+              setSong={(newSong: Song) => {
+                if (addedScore || song === undefined) {
+                  setSong(newSong);
+                  setSongSet(songSet.add(newSong));
+                  setAddedScore(false);
+                }
+              }}
+            ></RandomSongPicker>
+            <div className="challenge-container">
+              <p>{`Totaal: ${Object.values(totalScore).reduce((acc, e) => acc + e, 0)}/20`}</p>
+              {song ? (
+                <div className="score-button-container">
+                  <button onClick={createScoreHandler(2)}>2 punten</button>
+                  <button onClick={createScoreHandler(1)}>1 punt</button>
+                  <button onClick={createScoreHandler(0)}>0 punten</button>
+                </div>
+              ) : (
+                ""
+              )}
+              <div className="return-button-container">
+                <button className="return-button" onClick={goBack}>
+                  Terug
+                </button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="information-container">
+              <p>Welkom tot de codex challenge. </p>
+              <p>
+                Voor de challenge wordt gevraagd om de eerste strofe en het
+                refrein te zingen zonder codex, tenzij anders vermeld.
+              </p>
+              <p>
+                Elk lied staat op 2 punten en er worden 10 liedjes gevraagd. Je
+                kan dus 20 punten in totaal scoren.
+              </p>
+              <p>Je slaagt als je minstens 10 op 20 haalt.</p>
+              <p>
+                Er worden 4 nederland-, 3 frans-, 2 duits- en 1 anders- of
+                engelstalige liedjes gevraagd.
+              </p>
+            </div>
+            <div className="button-container">
+              <button
+                className="challenge-button"
+                onClick={(_) => setMode(Mode.CodexChallenge)}
+              >
+                Challenge starten
+              </button>
+              <button
+                className="randomizer-button"
+                onClick={(_) => setMode(Mode.Randomizer)}
+              >
+                Oefenen
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
--- src/components/codex/challenge/song-picker/RandomSongPicker.tsx
+++ src/components/codex/challenge/song-picker/RandomSongPicker.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { type Song } from "../../../../lib/codex-challenge";
+import { Language, type Song } from "../../../../lib/codex-challenge";
 import "./random-song-picker.css";
 
 interface Props {
@@ -8,7 +8,35 @@ interface Props {
   germanSongs: Song[];
   otherSongs: Song[];
   song?: Song;
+  alreadySelectedSongs?: Set<Song>;
   setSong: (s: Song) => void;
+}
+
+function alreadySelected(newSong: Song, currentSong?: Song) {
+  if (currentSong === undefined) return false;
+  return currentSong.title === newSong.title;
+}
+
+const limit: { [key in Language]: number } = {
+  [Language.DUTCH]: 4,
+  [Language.FRENCH]: 3,
+  [Language.GERMAN]: 2,
+  [Language.ENGLISH]: 1,
+  [Language.OTHER]: 1,
+};
+
+function filterLang(language: Language) {
+  return function f(song: Song) {
+    switch (language) {
+      case Language.ENGLISH:
+      case Language.OTHER:
+        return (
+          song.language === Language.ENGLISH || song.language === Language.OTHER
+        );
+      default:
+        return song.language === language;
+    }
+  };
 }
 
 export default function RandomSongPicker({
@@ -17,57 +45,133 @@ export default function RandomSongPicker({
   germanSongs,
   otherSongs,
   song,
+  alreadySelectedSongs,
   setSong,
 }: Props) {
-  console.log("Loaded");
+  const [error, setError] = useState<boolean>(false);
+
   function makeRandomFunction(langlist: Song[]) {
     return (_: React.MouseEvent<HTMLImageElement>) => {
-      let randIdx;
-      do {
-        randIdx = Math.floor(Math.random() * langlist.length);
-      } while (song !== undefined && langlist[randIdx].title == song.title);
-      setSong(langlist[randIdx]);
+      if (
+        alreadySelectedSongs !== undefined &&
+        [...alreadySelectedSongs].filter(filterLang(langlist[0].language))
+          .length === limit[langlist[0].language]
+      )
+        setError(true);
+      else {
+        setError((prev) => (!prev ? prev : !prev));
+        let randIdx;
+        do {
+          randIdx = Math.floor(Math.random() * langlist.length);
+        } while (
+          alreadySelectedSongs === undefined
+            ? song !== undefined && song.title === langlist[randIdx].title
+            : song !== undefined && alreadySelectedSongs.has(langlist[randIdx])
+        );
+        setSong(langlist[randIdx]);
+      }
     };
   }
 
   return (
     <div className="picker">
       <div className="buttons">
-        <img
-          className="lang"
-          src="/vlaamse-leeuw.jpg"
-          onClick={makeRandomFunction(dutchSongs)}
-          alt="vlaamse leeuw"
-        ></img>
-        <img
-          className="lang"
-          src="/waalse-haan.png"
-          onClick={makeRandomFunction(frenchSongs)}
-          alt="waalse haan"
-        ></img>
-        <img
-          className="lang"
-          src="/duitse-dinges.png"
-          onClick={makeRandomFunction(germanSongs)}
-          alt="duitse gemeenschap logo"
-        ></img>
-        <img
-          className="lang"
-          src="/internationaal.png"
-          onClick={makeRandomFunction(otherSongs)}
-          alt="anders"
-        ></img>
+        <div className="lang-container">
+          <img
+            className="lang"
+            src="/vlaamse-leeuw.jpg"
+            onClick={makeRandomFunction(dutchSongs)}
+            alt="vlaamse leeuw"
+          ></img>
+          {alreadySelectedSongs === undefined ? (
+            ""
+          ) : (
+            <p>
+              {`${
+                [...alreadySelectedSongs].filter(
+                  (s) => s.language === Language.DUTCH,
+                ).length
+              }/4`}
+            </p>
+          )}
+        </div>
+        <div className="lang-container">
+          <img
+            className="lang"
+            src="/waalse-haan.png"
+            onClick={makeRandomFunction(frenchSongs)}
+            alt="waalse haan"
+          ></img>
+          {alreadySelectedSongs === undefined ? (
+            ""
+          ) : (
+            <p>
+              {`${
+                [...alreadySelectedSongs].filter(
+                  (s) => s.language === Language.FRENCH,
+                ).length
+              }/3`}
+            </p>
+          )}
+        </div>
+        <div className="lang-container">
+          <img
+            className="lang"
+            src="/duitse-dinges.png"
+            onClick={makeRandomFunction(germanSongs)}
+            alt="duitse gemeenschap logo"
+          ></img>
+          {alreadySelectedSongs === undefined ? (
+            ""
+          ) : (
+            <p>
+              {`${
+                [...alreadySelectedSongs].filter(
+                  (s) => s.language === Language.GERMAN,
+                ).length
+              }/2`}
+            </p>
+          )}
+        </div>
+        <div className="lang-container">
+          <img
+            className="lang"
+            src="/internationaal.png"
+            onClick={makeRandomFunction(otherSongs)}
+            alt="anders"
+          ></img>
+          {alreadySelectedSongs === undefined ? (
+            ""
+          ) : (
+            <p>
+              {`${
+                [...alreadySelectedSongs].filter(
+                  (s) =>
+                    s.language === Language.ENGLISH ||
+                    s.language === Language.OTHER,
+                ).length
+              }/1`}
+            </p>
+          )}
+        </div>
       </div>
       <div className="result">
-        {song ? (
-          <div className="song">
-            <p>Titel: {song.title}</p>
-            <p>Pagina: {song.page}</p>
-            {song.description ? <p>{song.description}</p> : ""}
-          </div>
-        ) : (
-          ""
-        )}
+        <div className="song">
+          {error ? (
+            <>
+              <p>Je hebt het maximum aantal liedjes van deze taal al berijkt</p>
+              <p>Kies een andere taal </p>
+            </>
+          ) : song ? (
+            <>
+              <p>Titel: {song.title}</p>
+              <p>Pagina: {song.page}</p>
+              {song.description ? <p>{song.description}</p> : ""}
+            </>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
