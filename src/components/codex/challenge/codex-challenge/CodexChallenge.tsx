259bb6dae76ca4f6dfc777b08b7cdc97ee256355
Gérard Lichtert <62557229+thebaconfate@users.noreply.github.com>
2025-07-14 15:59:55 +0200

    added styles

--- src/components/codex/challenge/codex-challenge/CodexChallenge.tsx
+++ src/components/codex/challenge/codex-challenge/CodexChallenge.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { Song } from "../../../../lib/codex-challenge";
 import RandomSongPicker from "../song-picker/RandomSongPicker";
+import "./codex-challenge.css";
 
 interface Props {
   dutchSongs: Song[];
@@ -23,7 +24,6 @@ export default function CodexChallenge({
   const [mode, setMode] = useState<Mode | undefined>();
   const [score, setScore] = useState<number>(0);
   const [song, setSong] = useState<Song | undefined>(undefined);
-
 
   return (
     <>
@@ -48,12 +48,39 @@ export default function CodexChallenge({
           ></RandomSongPicker>
         </>
       ) : (
-        <>
-                            <div>
-                                <button onClick={(_) => setMode(Mode.CodexChallenge)}>Challenge starten</button>
-
-                            </div>
-                        </>
+        <div className="content-container">
+          <div className="information-container">
+            <h1 className="title">Codex Challenge</h1>
+            <p>Welkom tot de codex challenge </p>
+            <p>
+              Voor de challenge wordt gevraagd om de eerste strofe en het
+              refrein te zingen zonder codex, tenzij anders vermeld.
+            </p>
+            <p>
+              Elk lied staat op 2 punten en er worden 10 liedjes gevraagd. Je
+              kan dus 20 punten in totaal scoren.
+            </p>
+            <p>Je slaagt als je minstens 10 op 20 haalt.</p>
+            <p>
+              Er worden 4 nederland-, 3 frans-, 2 duits- en 1 anders- of
+              engelstalige liedjes gevraagd.
+            </p>
+          </div>
+          <div className="button-container">
+            <button
+              className="challenge-button"
+              onClick={(_) => setMode(Mode.CodexChallenge)}
+            >
+              Challenge starten
+            </button>
+            <button
+              className="randomizer-button"
+              onClick={(_) => setMode(Mode.Randomizer)}
+            >
+              Oefenen
+            </button>
+          </div>
+        </div>
       )}
     </>
   );
