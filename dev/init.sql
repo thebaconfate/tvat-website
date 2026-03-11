a00fe52ae6273121be8663bdfcd88ea2a2b04fb6
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-03-11 03:56:12 +0100

    moved some stuff around

--- dev/init.sql
+++ dev/init.sql
@@ -1,3 +1,10 @@
 /* For development purposes only */
-CREATE DATABASE IF NOT EXISTS tvat;
-USE tvat;
+-- Create database if it does not exist
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'tvat') THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE tvat');
+   END IF;
+END
+$do$;
