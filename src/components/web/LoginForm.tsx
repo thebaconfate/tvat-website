c96a62a26da51f58f99c28548b47350a37dc0d96
Gérard Lichtert <gerard.lichtert@hotmail.com>
2026-03-15 00:37:04 +0100

    WIP loginform

--- src/components/web/LoginForm.tsx
+++ src/components/web/LoginForm.tsx
@@ -1,4 +1,35 @@
-import style from "./LoginForm.module.css";
+import { credentialsSchema } from "@/lib/services/auth/auth.schemas";
+//import style from "./LoginForm.module.css";
+import { useForm } from "@tanstack/react-form";
+
 export default function LoginForm() {
-  return <></>;
+  const form = useForm({
+    defaultValues: {
+      password: "",
+      email: "",
+    },
+    validators: { onSubmit: credentialsSchema },
+  });
+  return (
+    <div>
+      <form method="POST">
+        <form.Field name="email">
+          {(field) => (
+            <div>
+              <label htmlFor={field.name}>{field.name}</label>
+              <input type="email" value={field.state.value} />
+            </div>
+          )}
+        </form.Field>
+        <form.Field name="password">
+          {(field) => (
+            <div>
+              <label htmlFor={field.name}>{field.name}</label>
+              <input type="password" value={field.state.value} />
+            </div>
+          )}
+        </form.Field>
+      </form>
+    </div>
+  );
 }
